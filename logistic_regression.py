Fixed overflow error (no more NaN in weights) and added initial debugging tools

logistic_regression.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-#Produces a normalized exponential output (softmax) for each class
+#Produces a normalized exponential output (softmax) for each class (K classes in total)
+# Inputs
+#   w = weights (K,W)
+#   x = features (W,N)
+#### Issue with overflow from Exponential of activation z ####
 
 def prediction(w,x):
     z = np.dot(w,x)    # Activations
@@ -11,32 +15,25 @@ def prediction(w,x):
 
 #Performs weight optimization (Gradient Descent for this case)
 # Inputs:
-#   w = weights
-#   x = features
+#   w = weights  (K,W)
+#   x = features (W,N)
 #   t = 1-of-k encoding data labels
 
 def train(w,x,t):
-    #while error >= 10:
+    x = np.array(x,dtype=np.float128)
+    n = x.shape[1]
+    x_trans = np.transpose(x)
+
     for i in range(100):
         print(i)
         y = prediction(w, x)
-        print("y shape")
-        print(y.shape)
-        print("t shape")
-        print(t.shape)
         error = y - t
 
-        #print("error shape")
-        #print(error.shape)
-        dw = np.dot(error, np.transpose(x))
-        # dw is actually sum of (error * x_of_jth weight) for every N training example
-        #x_copy = np.array()
-        #dw = np.sum(dotted, axis=1)
-        w += 0.1 * dw
+        dw = np.dot(error, x_trans) / n
+        w -= 0.1 * dw
     return w
 
 def standardize(x):
-    means = np.mean(x, axis=0,keepdims=True)
-    sd = np.std(x,axis=0,keepdims=True)
-    print (x - means)/sd
+    means = np.mean(x, axis=1,keepdims=True)
+    sd = np.std(x,axis=1,keepdims=True)
     return (x - means)/sd